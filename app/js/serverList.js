acb43a0f4d73b9aa3bb43dc468b2bb92a0bd548e
BKDEVTELENET <BKDEVTELENET>
2014-10-31 16:19:11 +0100

    [jboone] view as table

--- app/js/serverList.js
+++ app/js/serverList.js
@@ -1,31 +1,28 @@
 'use strict';
+/** applicationWithServerList: Configure your server */
+
+var bot =  {
+				name: 'BOT',
+				servers: [
+					{url: "http://best-offer-tool-advisor-next.apps.tln-dev.cegeka.be/bestoffer/status/"},	
+					{url: "http://best-offer-tool-advisor.apps.tln-dev.cegeka.be/bestoffer/status/"},	
+					{url: "http://apps.servicessup.telenet.be/bestoffer/status/"},	
+					{url: "http://apps.servicesint.telenet.be/bestoffer/status/"},	
+					{url: "http://apps.servicesuat.telenet.be/bestoffer/status/"},	
+					{url: "http://apps.servicestrg.telenet.be/bestoffer/status/"},	
+				]			
+			}
+
+var mijntelenet= {
+				name: 'Mijn Telenet',
+				servers: [
+					{url: "http://mijn.sup.telenet.be/mijntelenet/rest/status/"},
+					{url: "http://mijn.int.telenet.be/mijntelenet/rest/status/"},
+					{url: "http://mijn.uat.telenet.be/mijntelenet/rest/status/"},
+					{url: "http://mijn.trg.telenet.be/mijntelenet/rest/status/"}
+				]   
+			}
 
 var applicationWithServerList = {
-			BOT: {
-				name: 'BOT',
-				servers: {
-					botcegeka: {
-						url: "http://best-offer-tool-advisor-next.apps.tln-dev.cegeka.be/bestoffer/status/"
-					}	
-				}			
-			},
-			mijntelenet: {
-				name: 'Mijn Telenet',
-				servers: {
-					sup: {
-						url: "http://mijn.sup.telenet.be/mijntelenet/rest/status/"
-					},
-					int: {
-						url: "http://mijn.int.telenet.be/mijntelenet/rest/status/"
-					},
-					uat: {
-						url: "http://mijn.uat.telenet.be/mijntelenet/rest/status/"
-					},
-					trg: {
-						url: "http://mijn.trg.telenet.be/mijntelenet/rest/status/"
-					}
-
-				}   
-			} 
-			 
+			BOT : bot			 
 		};
